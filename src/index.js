471580a38485fbb56c081515cb0ee8906dbd74c7
Jeff Klaus <jpk1215@gmail.com>
2019-06-11 23:19:59 -0400

    routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import 'dotenv/config';
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import routes from './routes'; 
 
-console.log('Hello Node.js project.');
+const app = express();
 
-console.log(process.env.MY_SECRET);
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/event', routes.event);
+app.use('/user', routes.user);
+app.use('/guest', routes.guest);
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import event from './event';
+import user from './user';
+import guest from './guest';
+
+export default {
+  event,
+  user,
+  guest,
+};
